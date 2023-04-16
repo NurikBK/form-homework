1f54bb9427661765c9f4b146f8b9171b7b649e6b
Nurzhan Bizhan <bizhannk@gmail.com>
2023-04-16 11:53:46 +0600

    create

---
+++ src/components/form/CheckBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useField } from 'formik';
+
+const CheckBox = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
+  return (
+    <div>
+      <label className="checkbox-input">{children}</label>
+      <input type="checkbox" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export default CheckBox;
---
+++ src/components/form/RadioButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useField } from 'formik';
+
+const RadioButton = ({ label, ...props }) => {
+  const [field] = useField(props);
+
+  return (
+    <div>
+      <input
+        type="radio"
+        {...field}
+        {...props}
+        checked={field.value === props.value}
+      />
+      <label htmlFor={props.id || props.name}>{label}</label>
+    </div>
+  );
+};
+
+export default RadioButton;
