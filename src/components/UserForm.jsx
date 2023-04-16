b3970c07917b04c7c9918c880c1f6b5f35f555f8
Nurzhan Bizhan <bizhannk@gmail.com>
2023-04-16 16:40:43 +0600

    add all components

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import {
+  CCheckBox,
+  CRadioButton,
+  CSelectOptions,
+  CTextInput,
+  CTextarea,
+  colorOptions,
+  initialValues,
+  saucesOptions,
+  stoogeOptions,
+  validationSchema,
+} from './form';
+
+const UserForm = () => {
+  return (
+    <div className="container">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { resetForm }) => {
+          alert(JSON.stringify(values, null, 2));
+          resetForm();
+        }}
+      >
+        {({ values, handleSubmit, handleReset }) => (
+          <Form className="form-container" onSubmit={handleSubmit}>
+            <CTextInput
+              label="First Name"
+              name="firstName"
+              type="text"
+              placeholder="Enter your name"
+            />
+
+            <CTextInput
+              label="Last Name"
+              name="lastName"
+              type="text"
+              placeholder="Enter your last name"
+            />
+
+            <CTextInput
+              label="Age"
+              name="age"
+              type="number"
+              placeholder="Enter your age"
+            />
+            <div className="wrapper">
+              <p className="title">Employed</p>
+              <CCheckBox name="employed" />
+            </div>
+
+            <CSelectOptions label="Favorite color" name="favoriteColor">
+              {colorOptions.map((item) => (
+                <option key={item.label} value={item.value.toUpperCase()}>
+                  {item.value}
+                </option>
+              ))}
+            </CSelectOptions>
+            <div className="wrapper">
+              <p className="title">Sauces</p>
+              <div>
+                {saucesOptions.map((item) => (
+                  <CCheckBox
+                    key={item.label}
+                    name="souces"
+                    id={item.label}
+                    label={item.label}
+                    value={item.value}
+                  >
+                    {item.value}
+                  </CCheckBox>
+                ))}
+              </div>
+            </div>
+
+            <div className="wrapper">
+              <p className="title">Best Stooge</p>
+              <div>
+                {stoogeOptions.map((item) => (
+                  <CRadioButton
+                    key={item.label}
+                    name="stooge"
+                    id={item.label}
+                    value={item.value}
+                    label={item.label}
+                  />
+                ))}
+              </div>
+            </div>
+            <CTextarea name="notes" label="Notes" />
+            <div className="btn-wrapper">
+              <button type="submit" className="btn btn-submit">
+                Submit
+              </button>
+              <button
+                type="reset"
+                className="btn btn-reset"
+                onClick={handleReset}
+              >
+                Reset
+              </button>
+            </div>
+            <div className="result">
+              <textarea
+                cols={40}
+                rows={15}
+                value={JSON.stringify(values, null, 2)}
+                readOnly
+              />
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default UserForm;
