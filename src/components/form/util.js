d5820f5e736590aad4373727c194ff03e2b5f0af
Nurzhan Bizhan <bizhannk@gmail.com>
2023-04-16 16:40:17 +0600

    create util file

---
+++ src/components/form/util.js
@@ -0,0 +1,32 @@
+export const stoogeOptions = [
+  { label: 'Larry', value: 'Larry' },
+  { label: 'Moe', value: 'Moe' },
+  { label: 'Curly', value: 'Curly' },
+];
+
+export const saucesOptions = [
+  { label: 'Ketchup', value: 'Ketchup' },
+  { label: 'Mustard', value: 'Mustard' },
+  { label: 'Mayonnaise', value: 'Mayonnaise' },
+  { label: 'Guacamole', value: 'Guacamole' },
+];
+
+export const colorOptions = [
+  { label: 'Select your color', value: 'Select your color' },
+  { label: 'Red', value: 'red' },
+  { label: 'Blue', value: 'blue' },
+  { label: 'Green', value: 'green' },
+  { label: 'Yellow', value: 'yellow' },
+  { label: 'Purple', value: 'purple' },
+];
+
+export const initialValues = {
+  firstName: '',
+  lastName: '',
+  age: '',
+  employed: false,
+  favoriteColor: '',
+  souces: [],
+  stooge: 'Larry',
+  notes: '',
+};
