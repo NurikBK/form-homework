3f39fa248b653ccbe38333dd4c31e0b978fddbac
Nurzhan Bizhan <bizhannk@gmail.com>
2023-04-16 16:39:33 +0600

    create index files

---
+++ src/components/form/index.jsx
@@ -0,0 +1,7 @@
+export * from './CCheckBox';
+export * from './CRadioButton';
+export * from './CSelectOptions';
+export * from './CTextInput';
+export * from './CTextarea';
+export * from './validationSchema';
+export * from './util';
